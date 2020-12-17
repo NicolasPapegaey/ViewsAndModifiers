885bcd84e9453036fb6cd1fdb26da7efdf06ba25
Nicolas Papegaey <nicolas.papegaey@gmail.com>
2020-12-17 08:30:40 +0100

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Nicolas Papegaey on 17/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
